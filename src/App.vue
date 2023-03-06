585c817795c813895b7fb466f46e2a251b87c9a4
Beltrán Garcia Balbuena <beltranbalbuena@gmail.com>
2023-03-06 11:32:06 +0100

    step(1): add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,90 @@
 <template>
   <header class="header">
-    <SearchInput v-on:search="setQuery"/>
+    <SearchInput @search="setQuery" />
   </header>
   <div class="container">
     <aside class="facets">
       <h2>Filters</h2>
       <span>Status</span>
-      <BaseFilterList v-bind:filters="filters.status" v-slot="slotProps">
-        <StatusFilter v-on:clickCheckbox="changeCheckbox(slotProps.filter)">{{ slotProps.filter }}</StatusFilter>
+      <BaseFilterList :filters="filters.status" v-slot="slotProps">
+        <StatusFilter @clickCheckbox="changeCheckbox(slotProps.filter)">{{
+          slotProps.filter
+        }}</StatusFilter>
       </BaseFilterList>
     </aside>
     <main>
       <BaseGrid>
-        <CharacterCard v-for="character in characters" v-bind:key="character.id" v-bind:character="character"/>
+        <CharacterCard v-for="character in characters" :key="character.id" :character="character" />
       </BaseGrid>
     </main>
   </div>
 </template>
 <script lang="js">
-  import BaseFilterList from '@/components/BaseFilterList.vue';
-  import BaseGrid from '@/components/BaseGrid.vue';
-  import CharacterCard from '@/components/CharacterCard.vue';
-  import SearchInput from '@/components/SearchInput.vue';
-  import StatusFilter from '@/components/StatusFilter.vue';
+import BaseFilterList from '@/components/BaseFilterList.vue';
+import BaseGrid from '@/components/BaseGrid.vue';
+import CharacterCard from '@/components/CharacterCard.vue';
+import SearchInput from '@/components/SearchInput.vue';
+import StatusFilter from '@/components/StatusFilter.vue';
 
-  export default {
-    components: {
-      SearchInput,
-      CharacterCard,
-      StatusFilter,
-      BaseFilterList,
-      BaseGrid
-    },
-    data() {
-      return {
-        characters: [],
-        query: '',
-        status: '',
-        url: 'https://rickandmortyapi.com/api/character/',
-        /*
-         * HACK:
-         *  In Empathy Platform every request returns you the filters available, as Rick-&-morty API do not retrieve it we hardcode them here.
-         */
-        filters: {
-          status: ['unknown', 'Alive', 'Dead']
-        }
-      };
-    },
-    watch: {
-      query() {
-        this.search();
-      },
-      status() {
-        this.search();
+export default {
+  components: {
+    SearchInput,
+    CharacterCard,
+    StatusFilter,
+    BaseFilterList,
+    BaseGrid
+  },
+  data() {
+    return {
+      characters: [],
+      query: '',
+      status: '',
+      url: 'https://rickandmortyapi.com/api/character/',
+      /*
+       * HACK:
+       *  In Empathy Platform every request returns you the filters available, as Rick-&-morty API do not retrieve it we hardcode them here.
+       */
+      filters: {
+        status: ['unknown', 'Alive', 'Dead']
       }
+    };
+  },
+  watch: {
+    query() {
+      this.search();
     },
-    methods: {
-      setQuery(query) {
-        this.query = query;
-      },
-      search() {
-        fetch(this.url + '?name=' + this.query + (this.status ? '&status=' + this.status : '')).then(response => response.json())
-            .then(data => {
-              this.characters = data.results;
-              console.log(this.characters);
-            });
-      },
-      changeCheckbox(checkboxValue) {
-        if (this.status === checkboxValue) {
-          this.status = '';
-        } else {
-          this.status = checkboxValue;
-        }
+    status() {
+      this.search();
+    }
+  },
+  methods: {
+    setQuery(query) {
+      this.query = query;
+    },
+    search() {
+      fetch(this.url + '?name=' + this.query + (this.status ? '&status=' + this.status : '')).then(response => response.json())
+          .then(data => {
+            this.characters = data.results;
+            console.log(this.characters);
+          });
+    },
+    changeCheckbox(checkboxValue) {
+      if (this.status === checkboxValue) {
+        this.status = '';
+      } else {
+        this.status = checkboxValue;
       }
     }
-  };
+  }
+};
 </script>
 <style scoped>
-  .header {
-    display: flex;
-    justify-content: center;
-  }
+.header {
+  display: flex;
+  justify-content: center;
+}
 
-  .container {
-    display: flex;
-  }
+.container {
+  display: flex;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Create a new store instance.
+const store = createStore({})
+
+createApp(App).use(store).mount('#app')
